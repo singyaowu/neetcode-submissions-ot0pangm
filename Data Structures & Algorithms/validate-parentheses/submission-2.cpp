acaed8d7dbae8200ef20688215f23a0208fd13d1
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-07 16:43:13 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (auto c: s) {
+            switch(c) {
+                case '(':
+                case '{':
+                case '[':
+                    stk.push(c);
+                    break;
+                case ')':
+                    if (stk.empty() || stk.top() != '(') return false;
+                    stk.pop();
+                    break;
+                case ']':
+                    if (stk.empty() || stk.top() != '[') return false;
+                    stk.pop();
+                    break;
+                case '}':
+                    if (stk.empty() || stk.top() != '{') return false;
+                    stk.pop();
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return stk.empty();
+    }
+};
