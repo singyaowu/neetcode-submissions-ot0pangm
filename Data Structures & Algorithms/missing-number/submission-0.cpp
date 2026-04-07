1bed4df215ee45229e206415598ede28a71046b9
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-07 10:16:54 -0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = nums.size() * (nums.size() + 1) / 2;
+        for (auto n: nums) {
+            sum -= n;
+        }
+        return sum;
+    }
+};
