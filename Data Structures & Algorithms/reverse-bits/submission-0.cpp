e7cd0323515fdb90fa3c52184be18c06dd11333f
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-07 10:07:05 -0700

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int ret = 0;
+        for(int i = 0; i < 32; i++) {
+            if (i > 0) {
+                ret <<= 1;
+                n >>= 1;
+            }
+            ret += n&1;
+        }
+        return ret;
+    }
+};
