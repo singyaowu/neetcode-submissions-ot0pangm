32e0d2c183f16f849763e8dc60e96248ed3747be
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-09 21:21:09 -0700

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int i = 0;
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        res.push_back(newInterval);
+        for (; i < intervals.size(); i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[0] = min(intervals[i][0], res.back()[0]);
+                res.back()[1] = max(intervals[i][1], res.back()[1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
