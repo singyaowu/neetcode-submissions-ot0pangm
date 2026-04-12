4cd223d8141da80a0820654af0203caa81199160
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-12 00:11:27 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<int, Node*> visit;
+        return dfs(visit, node);
+    }
+    Node* dfs(unordered_map<int, Node*> &visit, Node* node) {
+        if (!node) return nullptr;
+        if (visit.count(node->val))
+            return visit[node->val];
+
+        int val = node->val;
+        auto copy = new Node(val);
+        visit[val] = copy;
+        for (auto neib : node->neighbors) {
+            copy->neighbors.push_back(dfs(visit, neib));
+        }
+        return copy;
+    }
+};
