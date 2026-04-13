e08e0cb69df535c3574237ced7849faa5bd68f5e
Sing-Yao Wu <r07922004@ntu.edu.tw>
2026-04-12 21:21:39 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> kv_map;
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        kv_map[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (!kv_map.count(key))
+            return "";
+
+        auto &timeline = kv_map[key];
+        int l = 0, r = timeline.size();
+        
+        while(l < r) {
+            int mid = l + (r-l) / 2;
+            if (timeline[mid].first > timestamp) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l == 0? "" : timeline[l-1].second;
+    }
+};
